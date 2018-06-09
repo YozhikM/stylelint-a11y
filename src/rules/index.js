92915bc0be088ece53258876de69b387271c653f
Igor Kamyshev <garik.novel@gmail.com>
2018-06-09 17:31:45 +0300

    Fixed sorting

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,13 +1,13 @@
 import fontSizeIsReadable from './font-size-is-readable';
 import mediaPrefersReducedMotion from './media-prefers-reduced-motion';
+import noDisplayNone from './no-display-none';
 import noOutlineNone from './no-outline-none';
 import selectorPseudoClassFocus from './selector-pseudo-class-focus';
-import noDisplayNone from './no-display-none';
 
 export default {
   'font-size-is-readable': fontSizeIsReadable,
   'media-prefers-reduced-motion': mediaPrefersReducedMotion,
+  'no-display-none': noDisplayNone,
   'no-outline-none': noOutlineNone,
   'selector-pseudo-class-focus': selectorPseudoClassFocus,
-  'no-display-none': noDisplayNone,
 };
