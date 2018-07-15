97859c427fd1e09982bbab884e44eac9f5055206
s.botev <botev.st@gmail.com>
2018-07-15 22:48:35 +0200

    Fix issue #31

--- src/rules/no-spread-text/index.js
+++ src/rules/no-spread-text/index.js
@@ -24,6 +24,7 @@ const textStyles = [
 const nodesProbablyForText = nodes =>
   nodes
     .map(node => node.prop)
+    .filter(Boolean)
     .map(prop => prop.toLowerCase())
     .some(prop => textStyles.includes(prop));
 
